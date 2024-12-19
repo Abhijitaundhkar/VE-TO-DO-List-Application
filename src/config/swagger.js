cc4fdd2c9423488bd6789b538c26ac5666b69523
Abhijit Aundhkar <abhijitaundhkar@gmail.com>
2024-12-19 20:04:46 +0530

    .

---
+++ src/config/swagger.js
@@ -0,0 +1,33 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const swaggerDefinition = {
+  openapi: "3.0.0", // Specifies the OpenAPI version
+  info: {
+    title: "TO DO API Documentation", // Title of your API
+    version: "1.0.0", // Version of your API
+    description: "This is the API documentation.", // Description
+  },
+  servers: [
+    {
+      url: "http://localhost:5000", // API server URL
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["../routes/*.js"], // Path to your API route files for JSDoc comments
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
